04809426f55652b27ed9a4c9c9254ac7cddf59ed
CarlosOlivera92 <olivera.carlos1999@gmail.com>
2024-05-26 00:51:42 -0300

    add countdown feature

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 import { header } from "./components/header/header.js";
-
+import { countdown } from "./components/countdown/countdown.js";
 document.addEventListener("DOMContentLoaded", () => {
     header();
+    countdown();
 })
