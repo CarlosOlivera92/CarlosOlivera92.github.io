556fbb770ff09bfc0ba4171d296a64fcf5d9e364
CarlosOlivera92 <olivera.carlos1999@gmail.com>
2024-05-27 18:48:41 -0300

    add gsap to manage dropdown animations

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 import { header } from "./components/header/header.js";
 import { countdown } from "./components/countdown/countdown.js";
+import { cardLarge } from "./components/cardLarge/cardLarge.js";
 document.addEventListener("DOMContentLoaded", () => {
     header();
     countdown();
+    cardLarge();
 })
